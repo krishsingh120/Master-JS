0e134b6a63ded0f7f5febd2f4257a271b6877646
Krish Singh <154358180+krishsingh120@users.noreply.github.com>
2026-01-18 18:59:43 +0530

    Feat: Added addition operator

---
+++ Coercion/additionOperator.js
@@ -0,0 +1,9 @@
+const obj = {};
+
+console.log(obj); // output = empty obj => {}
+
+console.log("18" + obj);   // "18" + "[obj, obj]" => concatenate
+
+console.log(18 + obj);  // 18 + "[obj, obj]" => same here concatenate
+
+
